a680f58e703e6842280fc42efe58ded3ac978ccf
Thomas Peterson <t.ripley.peterson@gmail.com>
2016-09-18 13:18:55 -0400

    Fix static config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8000
 
 const app = express()
 
-app.use(express.static('public'))
+app.use('/', express.static(`${__dirname}/public`))
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')
